9bbd2c64283f65f34ee2c2bc3acbd2933eb77476
cindy <ciindy.wu@gmail.com>
2020-11-08 18:15:44 -1000

    Adds hawaii grotto hero image to root

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        hawai'i grotto
+      </h1>
+      <img src="https://i.imgur.com/6bqaWRL.png" alt="Hawai'i Grotto Hero"></img>
+      <h3>
+        Subtitle
+      </h3>
     </div>
-  );
-}
+  )
+} 
 
 export default App;
