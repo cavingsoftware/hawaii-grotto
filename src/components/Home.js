c17fd91d27c124705d12d96e06250a4239e3e5db
cindy <ciindy.wu@gmail.com>
2020-11-08 22:28:22 -1000

    fix image alt text

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className="home">
-      <img className="center-fit" src="https://i.imgur.com/6bqaWRL.png"></img>
+      <img className="center-fit" src="https://i.imgur.com/6bqaWRL.png" alt="Hawai'i Grotto Hero"></img>
     </div>
   )
 }
